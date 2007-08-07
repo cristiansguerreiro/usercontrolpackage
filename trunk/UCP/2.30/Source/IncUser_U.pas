ce15d3c1ea0b339ef8cd7f828aa8bcdf51622f41
luiz.benevenuto <luiz.benevenuto@98b1ab29-1e28-0410-b393-7fe010e6005d>
2007-08-07 13:08:16 +0000

    UCmail ausente no repositório !

--- trunk/UCP/2.30/Source/IncUser_U.pas
+++ trunk/UCP/2.30/Source/IncUser_U.pas
@@ -138,13 +138,15 @@ begin
 
       AddUser(vLogin, vNovaSenha, vNome, vEmail, vPerfil, vUserExpired, SpinExpira.Value, vPrivilegiado);
 
-      if (Assigned( fUserControl.MailUserControl)) and (fUserControl.MailUserControl.AdicionaUsuario.Ativo ) then
-        try
-          fUserControl.MailUserControl.EnviaEmailAdicionaUsuario(vNome, vLogin, Encrypt(vNovaSenha, EncryptKey) , vEmail, IntToStr(vPerfil), EncryptKey);
-        except
-          on E : Exception do Log(e.Message, 0 );
-        end;
+(*
+        if (Assigned( fUserControl.MailUserControl)) and (fUserControl.MailUserControl.AdicionaUsuario.Ativo ) then
+          try
+            fUserControl.MailUserControl.EnviaEmailAdicionaUsuario(vNome, vLogin, Encrypt(vNovaSenha, EncryptKey) , vEmail, IntToStr(vPerfil), EncryptKey);
+          except
+            on E : Exception do Log(e.Message, 0 );
+          end;
 
+*)
 
     end
     else
@@ -162,13 +164,15 @@ begin
       vPrivilegiado := ckPrivilegiado.Checked;
       ChangeUser(vNovoIDUsuario, vLogin, vNome, vEmail, vPerfil, vUserExpired, SpinExpira.Value, ComboStatus.ItemIndex, vPrivilegiado);
 
-      if (Assigned(fUserControl.MailUserControl)) and (fUserControl.MailUserControl.AlteraUsuario.Ativo ) then
-        try
-          fUserControl.MailUserControl.EnviaEmailAlteraUsuario(vNome, vLogin, 'Não Alterada', vEmail, IntToStr(vPerfil), EncryptKey);
-        except
-          on E : Exception do Log(e.Message, 2);
-        end;
-
+(*
+        if (Assigned(fUserControl.MailUserControl)) and (fUserControl.MailUserControl.AlteraUsuario.Ativo ) then
+          try
+            fUserControl.MailUserControl.EnviaEmailAlteraUsuario(vNome, vLogin, 'Não Alterada', vEmail, IntToStr(vPerfil), EncryptKey);
+          except
+            on E : Exception do Log(e.Message, 2);
+          end;
+    
+*)
     end;
 
 {  With TfrmCadastrarUsuario(Owner) do
