be59148b0bffae5014f3583cf8a67f8bcee53013
luiz.benevenuto <luiz.benevenuto@98b1ab29-1e28-0410-b393-7fe010e6005d>
2007-08-08 17:54:19 +0000

    Retirado o registro do arquivo .config

--- UCP/2.30/clean.bat
+++ UCP/2.30/clean.bat
@@ -1,6 +1,6 @@
 @echo off
 echo erasing...
-del /S *.local *.dcu *.ddp *.dsk *.~* *.cfg *.dsm *.identcache *.dof *.stat *humbs.db
+del /S *.local *.dcu *.ddp *.dsk *.~* *.cfg *.dsm *.identcache *.dof *.stat *humbs.db *.config
 del ??history* /s /a
 
 
